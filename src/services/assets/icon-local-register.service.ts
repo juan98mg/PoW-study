d338429a265c829682a28309dca226a52e4bf0e5
Juan Stiven Muñoz Garcia <juan.98mg@gmail.com>
2023-06-29 10:56:30 -0500

    nuevos iconos

--- src/services/assets/icon-local-register.service.ts
+++ src/services/assets/icon-local-register.service.ts
@@ -2,20 +2,24 @@ import { Injectable } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IconLocalRegisterService {
-
   customIconList: Array<CustomIconItem> = [
-    {svgName: "hasher", svgPath: "./../../assets/icons/hash-key.svg" }
-  ]
+    { svgName: 'hasher', svgPath: './../../assets/icons/hash-key.svg' },
+    { svgName: 'block', svgPath: './../../assets/icons/block.svg' },
+    { svgName: 'blockchain', svgPath: './../../assets/icons/blockchain.svg' },
+    { svgName: 'mining', svgPath: './../../assets/icons/mining.svg' },
+    { svgName: 'coin', svgPath: './../../assets/icons/coin.svg' },
+    { svgName: 'clean', svgPath: './../../assets/icons/clean.svg' },
+    { svgName: 'check', svgPath: './../../assets/icons/check.svg' },
+  ];
 
-
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-  }
-
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {}
 
   async run(): Promise<void> {
     for (const newIcon of this.customIconList) {
@@ -34,8 +38,7 @@ export class IconLocalRegisterService {
   }
 }
 
-
 interface CustomIconItem {
-  svgName: string
-  svgPath: string
+  svgName: string;
+  svgPath: string;
 }
